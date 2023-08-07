93ff3b66e2af47a4c9d99c282920c4da070cd13c
Gitasawaaah <asawauno@gmail.com>
2023-08-06 22:32:47 -0400

    version finale en mode github pages

--- main.js
+++ main.js
@@ -3,10 +3,15 @@
     const birthdate = new Date("1991-11-08");
     const currentDate = new Date();
 
-    const hoursPerDay = 24;
-    const daysPassed = Math.floor((currentDate - birthdate) / hoursPerDay);
+    const millisecondsPerDay = 1000 * 60 * 60 * 24;
+    const daysPassed = Math.floor((currentDate - birthdate) / millisecondsPerDay);
 
     const yearsPassed = (daysPassed / 365).toFixed(1);
 
-    resultElement.textContent = `Nombre de jours passés depuis la naissance : ${daysPassed} jours\nNombre d'années passées : ${yearsPassed} ans`;
-});
+    const nombreJour = document.getElementById("nbjours");
+    nombreJour.textContent = daysPassed;
+    const nombreAnnee = document.getElementById("annee");
+    nombreAnnee.textContent = yearsPassed;
+
+console.log(`Nombre de jours passés depuis la naissance : ${daysPassed} jours`);
+console.log(`Nombre d'années passées : ${yearsPassed} ans`);
